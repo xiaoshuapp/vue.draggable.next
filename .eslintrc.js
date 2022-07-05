4f63d5386a3c6343a1d9558bface4d689d015e5a
greenhat616 <i@a632079.me>
2022-07-05 16:26:07 +0800

    fix: lint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {},
+  //highlight-start
+  overrides: [
+    {
+      files: "**/*.+(ts|tsx)",
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint/eslint-plugin"],
+      extends: [
+        "plugin:vue/vue3-essential",
+        "@vue/prettier",
+        "plugin:@typescript-eslint/eslint-recommended", // removes redundant warnings between TS & ESLint
+        "plugin:@typescript-eslint/recommended", // rules specific to typescript, e.g., writing interfaces
+      ],
+    },
+  ],
+};
